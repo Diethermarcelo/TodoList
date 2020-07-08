11d96657cd9130e9b3080eb611a5aff97d9be138
Diether <miguelitossss199@gmail.com>
2020-07-09 01:55:21 +0800

    Updates

---
+++ precache-manifest.3b9c3441d3adcecf67d96af9397c51d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73b6e2889b61c51d263a51ae6bac8dc",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "293b773583589f7866fa",
+    "url": "/TodoList/static/css/main.9e44f6c5.chunk.css"
+  },
+  {
+    "revision": "85e6e348f250be2e8c44",
+    "url": "/TodoList/static/js/2.2984d714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.2984d714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293b773583589f7866fa",
+    "url": "/TodoList/static/js/main.551535b9.chunk.js"
+  },
+  {
+    "revision": "23d45432894410375589",
+    "url": "/TodoList/static/js/runtime-main.d0f80f2c.js"
+  }
+]);
